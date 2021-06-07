e0f6127929b31d8c324555a9fa2163f1f313c05b
Derik Marco <38227342+xdxddxd@users.noreply.github.com>
2021-06-07 18:53:16 -0300

    Vendor

---
+++ tempVendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'xdxddxd/tabacariadelivery',
+  ),
+  'versions' => 
+  array (
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.7',
+      'version' => '1.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a51fa227bba003be11519e7efa8338bc87460f5',
+    ),
+    'coffeecode/optimizer' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6379bc47072e3e5d6354d51c5731ecab860804f3',
+    ),
+    'coffeecode/router' => 
+    array (
+      'pretty_version' => '1.0.7',
+      'version' => '1.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e3b94f72bc389e8b97d82a0fd0e5bff743e51667',
+    ),
+    'coffeecode/uploader' => 
+    array (
+      'pretty_version' => '1.0.10',
+      'version' => '1.0.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1fd58f13d41de26be36bef9fcebe54b865f3380e',
+    ),
+    'league/plates' => 
+    array (
+      'pretty_version' => 'v3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d3ee31199b536a4e003b34a356ca20f6f75496a',
+    ),
+    'xdxddxd/tabacariadelivery' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
