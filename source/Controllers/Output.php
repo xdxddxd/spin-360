9e1eebd0bf6823521d33a855f084c13bdbdd8d8f
Derik Marco <38227342+xdxddxd@users.noreply.github.com>
2021-06-15 05:10:31 -0300

    .

--- source/Controllers/Output.php
+++ source/Controllers/Output.php
@@ -4,63 +4,62 @@ namespace Source\Controllers;
 
 use Source\Models\Imagem;
 
-class Output{
-    
+class Output
+{
+
     public function Picture($data)
     {
         $image = new Imagem();
         $files = $_FILES;
 
         if (!empty($files["file"])) {
-             $file = $files["file"];
+            $file = $files["file"];
 
-             if (empty($file["type"]) || !in_array($file["type"], $image::isAllowed())) {
-                  echo json_encode([
-                       "code" => false,
-                       "title" => "Erro !",
-                       "message" => "Selecione Uma Imagem Válida !",
-                       "id" => uniqid()
-                  ]);
-             } else {
-                  $imgName = hash('sha256', $_SESSION['ADMIN']['USER']['ID'].'-'.uniqid());
+            if (empty($file["type"]) || !in_array($file["type"], $image::isAllowed())) {
+                echo json_encode([
+                    "code" => false,
+                    "title" => "Erro !",
+                    "message" => "Selecione Uma Imagem Válida !",
+                    "id" => uniqid()
+                ]);
+            } else {
+                $imgName = hash('sha256', $_SESSION['ADMIN']['USER']['ID'] . '-' . uniqid());
 
-                  if ($image->upload($file, $imgName, 450)) {
+                if ($image->upload($file, $imgName, 450)) {
 
-                       switch ($file['type']) {
-                            case "image/jpeg":
-                                 $ext = '.jpg';
-                                 break;
+                    switch ($file['type']) {
+                        case "image/jpeg":
+                            $ext = '.jpg';
+                            break;
 
-                            case "image/png":
-                                 $ext = '.png';
-                                 break;
+                        case "image/png":
+                            $ext = '.png';
+                            break;
 
-                            case "image/gif":
-                                 $ext = '.gif';
-                                 break;
+                        case "image/gif":
+                            $ext = '.gif';
+                            break;
 
-                            default:
-                                 $ext = ".err";
-                                 break;
-                       }
+                        default:
+                            $ext = ".err";
+                            break;
+                    }
 
-                       $_SESSION['ADMIN']['USER']['EMPRESA']['URL_IMG'] = URL_BASE . '/assets/uploads/images/' . date('Y') . '/' . date('m') . '/' . $imgName . $ext;
-
-                       echo json_encode([
-                            "code" => true,
-                            "title" => "Imagem Atualizada !",
-                            "message" => "<img src='" . URL_BASE . '/assets/uploads/images/' . date('Y') . '/' . date('m') . '/' . $imgName . $ext . "' class='uploaded-image' alt='Profile Picture'>",
-                            "id" => uniqid()
-                       ]);
-                  } else {
-                       echo json_encode([
-                            "code" => false,
-                            "title" => "Erro !",
-                            "message" => "Seu Arquivo é Muito Pesado !",
-                            "id" => uniqid()
-                       ]);
-                  }
-             }
+                    echo json_encode([
+                        "code" => true,
+                        "title" => "Imagem Atualizada !",
+                        "message" => "<img src='" . URL_BASE . '/assets/uploads/images/' . date('Y') . '/' . date('m') . '/' . $imgName . $ext . "' class='uploaded-image' alt='Profile Picture'>",
+                        "id" => uniqid()
+                    ]);
+                } else {
+                    echo json_encode([
+                        "code" => false,
+                        "title" => "Erro !",
+                        "message" => "Seu Arquivo é Muito Pesado !",
+                        "id" => uniqid()
+                    ]);
+                }
+            }
         }
-   }
+    }
 }
